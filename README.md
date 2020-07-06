45b3d46c6bc23d4fa8e4b5d03f64d7ec8be7a737
Juliver Napitupulu <juliverna70@gmail.com>
2020-07-06 16:10:13 +0200

    add result and other filter tools

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Image Processing
+
+## ToDo List
+[ ] Upload the result
+[ ] Write the step of image processing
--- color-segmentation/segmentHSV.py
+++ color-segmentation/segmentHSV.py
@@ -1,3 +1,4 @@
+# import the required libraries
 import cv2
 import numpy as np
 
@@ -8,77 +9,93 @@ def nothing(x):
 
 class SegmentHSV(object):
     def __init__(self):
-        self.capture = cv2.VideoCapture(0)
-
+        self.image_src = 'image'
         self.trackbar = np.zeros((200, 560, 3), np.uint8)
+        self.win_name = ['Input', 'HSV', 'Parameter', 'Mask', 'Transform', 'Open', 'Close', 'Output']
+        for win in self.win_name:
+            cv2.namedWindow(win, cv2.WINDOW_NORMAL)
+            cv2.resizeWindow(win, 400, 345)
+        self.switch = 'Show color'
         self.var_name = ['Hue min', 'Sat min', 'Val min', 'Hue max', 'Sat max', 'Val max']
-        self.win_name = 'Parameter'
-        self.switch = '0 : OFF \n 1 : ON'
+        self.kernel = ['Erode size', 'Dilate size', 'Open size', 'Close size']
+        if self.image_src == 'video_cam':
+            self.capture = cv2.VideoCapture(0)
+        elif self.image_src == 'image':
+            self.capture = cv2.imread('./images-baustelle/test4.jpg', 1)
 
     def trackbar_create(self):
-        cv2.namedWindow(self.win_name)
+        """ Create the trackbar and selected color """
+        cv2.namedWindow(self.win_name[2])
         for var in self.var_name:
-            cv2.createTrackbar(var, self.win_name, 0, 255, nothing)
-        cv2.createTrackbar('Erode kernel size', self.win_name, 0, 20, nothing)
-        cv2.createTrackbar('Dilate kernel size', self.win_name, 0, 20, nothing)
-        cv2.createTrackbar('Open kernel size', self.win_name, 0, 20, nothing)
-        cv2.createTrackbar('Close kernel size', self.win_name, 0, 20, nothing)
+            cv2.createTrackbar(var, self.win_name[2], 0, 255, nothing)
+        for kernel in self.kernel:
+            cv2.createTrackbar(kernel, self.win_name[2], 0, 20, nothing)
         cv2.createTrackbar(self.switch, 'Parameter', 0, 1, nothing)
 
     def rect_kernel(self, size):
+        """ Rectangular kernel """
         size = size + 1
         return cv2.getStructuringElement(cv2.MORPH_RECT, (size, size))
 
     def ellip_kernel(self, size):
+        """ Elliptical kernel """
         size = size + 1
         return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (size, size))
 
     def cross_kernel(self, size):
+        """ Cross-shaped kernel """
         size = size + 1
         return cv2.getStructuringElement(cv2.MORPH_CROSS, (size, size))
 
     def start(self):
         self.trackbar_create()
         while True:
+            shown_fig = []
             # capture current image
-            _, image = self.capture.read()
+            image = []
+            if self.image_src == 'video_cam':
+                _, image = self.capture.read()
+            elif self.image_src == 'image':
+                image = self.capture
+            shown_fig.append(image)
+
+            # convert color from RGB to HSV format
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            shown_fig.append(hsv)
 
-            # read parameters
-            h_min = cv2.getTrackbarPos('Hue min', 'Parameter')
-            s_min = cv2.getTrackbarPos('Sat min', 'Parameter')
-            v_min = cv2.getTrackbarPos('Val min', 'Parameter')
-            h_max = cv2.getTrackbarPos('Hue max', 'Parameter')
-            s_max = cv2.getTrackbarPos('Sat max', 'Parameter')
-            v_max = cv2.getTrackbarPos('Val max', 'Parameter')
-            swi_val = cv2.getTrackbarPos(self.switch, 'Parameter')
-            erosion_kern_size = cv2.getTrackbarPos('Erode kernel size', 'Parameter')
-            dilate_kern_size = cv2.getTrackbarPos('Dilate kernel size', 'Parameter')
-            open_kern_size = cv2.getTrackbarPos('Open kernel size', 'Parameter')
-            close_kern_size = cv2.getTrackbarPos('Close kernel size', 'Parameter')
-            erode_kernel = np.ones((erosion_kern_size, erosion_kern_size), np.uint8)
-            dilate_kernel = np.ones((dilate_kern_size, dilate_kern_size), np.uint8)
+            # read params
+            hsv_set = []
+            for var in self.var_name:
+                hsv_set.append(cv2.getTrackbarPos(var, self.win_name[2]))
+            switch_val = cv2.getTrackbarPos(self.switch, 'Parameter')
+            kernel_size = []
+            for kernel in self.kernel:
+                kernel_size.append(cv2.getTrackbarPos(kernel, 'Parameter'))
+            erode_kernel = np.ones((kernel_size[0], kernel_size[0]), np.uint8)
+            dilate_kernel = np.ones((kernel_size[1], kernel_size[1]), np.uint8)
 
-            if swi_val == 0:
+            # show the selected color if switch is on
+            if switch_val == 0:
                 self.trackbar[:] = 0
             else:
-                self.trackbar[:] = (np.array([h_min, s_min, v_min]) + np.array([h_max, s_max, v_max])) / 2
+                self.trackbar[:] = (np.array(hsv_set[0:3]) + np.array(hsv_set[3:])) / 2
                 self.trackbar = cv2.cvtColor(self.trackbar, cv2.COLOR_HSV2BGR)
+            shown_fig.append(self.trackbar)
 
             # color filter
-            lower_hsv_val = np.array([h_min, s_min, v_min])
-            upper_hsv_val = np.array([h_max, s_max, v_max])
-            res_col_img = cv2.inRange(hsv, lower_hsv_val, upper_hsv_val)
+            lower_hsv_val = np.array(hsv_set[0:3])
+            upper_hsv_val = np.array(hsv_set[3:])
+            result_hsv = cv2.inRange(hsv, lower_hsv_val, upper_hsv_val)
+            shown_fig.append(result_hsv)
 
-            # erode image
-            eroded_img = cv2.erode(res_col_img, erode_kernel)
-
-            # dilate image
+            # image processing
+            eroded_img = cv2.erode(result_hsv, erode_kernel)
             dilated_image = cv2.dilate(eroded_img, dilate_kernel)
-
-            # opening and closing image transformation
-            open_img = cv2.morphologyEx(dilated_image, cv2.MORPH_OPEN, self.rect_kernel(open_kern_size))
-            close_img = cv2.morphologyEx(open_img, cv2.MORPH_CLOSE, self.rect_kernel(close_kern_size))
+            shown_fig.append(dilated_image)
+            open_img = cv2.morphologyEx(dilated_image, cv2.MORPH_OPEN, self.rect_kernel(kernel_size[2]))
+            shown_fig.append(open_img)
+            close_img = cv2.morphologyEx(open_img, cv2.MORPH_CLOSE, self.rect_kernel(kernel_size[3]))
+            shown_fig.append(close_img)
 
             # find contours
             contours, hierarchy = cv2.findContours(close_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -89,35 +106,37 @@ class SegmentHSV(object):
             except TypeError:
                 hierarchy = []
 
+            # get the dimension of image
             height, width = close_img.shape
             min_x, min_y = width, height
             max_x = max_y = 0
 
+            # define the box location and dimension
             output_img = image.copy()
             for contour, _ in zip(contours, hierarchy):
                 (x, y, w, h) = cv2.boundingRect(contour)
                 min_x, max_x = min(x, min_x), max(x + w, max_x)
                 min_y, max_y = min(y, min_y), max(y + h, max_y)
-                if w > 80 and h > 80:
+                if w > 50 and h > 50:
                     cv2.rectangle(output_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             if max_x - min_x > 0 and max_y - min_y > 0:
                 cv2.rectangle(output_img, (min_x, min_y), (max_x, max_y), (255, 0, 0), 2)
+            if contours:
+                # show the total area of contour
+                cv2.putText(output_img, 'Area of interest : {}'.format(cv2.contourArea(contours[0])), (50, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 8)
+            shown_fig.append(output_img)
 
-            # displays
-            cv2.imshow('Parameter', self.trackbar)
-            cv2.imshow('Input', image)
-            cv2.imshow('HSV', hsv)
-            cv2.imshow('Mask', res_col_img)
-            cv2.imshow('Morph', dilated_image)
-            cv2.imshow('Open', open_img)
-            cv2.imshow('Close', close_img)
-            cv2.imshow('Output', output_img)
+            # result/output display
+            for i, win in enumerate(self.win_name):
+                cv2.imshow(win, shown_fig[i])
 
             # wait until 'q' is touched
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+        # release capture and close all windows
         self.capture.release()
         cv2.destroyAllWindows()
 
